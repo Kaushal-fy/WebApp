0a7eacfc578075204211bbfda690cdc54464175e
Kaushal <kaushalthakur1992@gmail.com>
2020-09-08 03:53:30 +0530

    Modified for proper format

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $client1 = file_get_contents('http://34.239.126.215/');
 $client2 = file_get_contents('http://54.84.195.147/');
-echo "Server1 " . $client1 . "<br>";
-echo "Server2 " . $client2 . "<br>";
+echo $client1 . "<br>";
+echo $client2 . "<br>";
 ?>
