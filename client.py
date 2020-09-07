e9e5ad6681e372e5d29ab34958a2fb3f0b775bf1
Kaushal <kaushalthakur1992@gmail.com>
2020-09-08 04:07:14 +0530

    Added hostname to script, redirected o/p to index.html

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
+import socket
 import sys
 import time
 while True:
     time.sleep(1)
+    host=socket.gethostname()
     file=open("/var/log/secure", "r")
     count=0
     for line in file:
         if 'Failed' in line or 'Accepted' in line:
             count=count+1
-#    sys.stdout = open('file', 'w')
     sys.stdout = open("/var/www/html/index.html", 'w')
-    print("No of SSH attempts on server: ",count)
+    print(str(count) + " SSH attempts happened on " + host)
